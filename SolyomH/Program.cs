e3a4b18399475546171c2953b0376c180a29ef10
susyhuni <solyomh@kkszki.hu>
2025-09-25 09:02:24 +0200

    fel.2

--- SolyomH/Program.cs
+++ SolyomH/Program.cs
@@ -10,6 +10,44 @@ namespace SolyomH
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Adja megy az első számot");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Adja megy a második számot");
+            int y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+            {
+                Console.WriteLine($"A {x} nagyobb mint {y}, {x - y - 1}");
+
+            }
+            else if (y > x)
+            {
+                Console.WriteLine($"A {y} nagyobb mint {x}, {y - x - 1}");
+
+            }
+            else
+            { Console.WriteLine("Nem jó adatot adott meg"); }
+
+            {
+                int szam = 1;
+
+                List<int> list = new List<int>();
+                while (szam != 0)
+
+                {
+                    Console.WriteLine($"Adjon meg számot(0)");
+                    szam = int.Parse(Console.ReadLine());
+                    list.Add(szam);
+                }
+                var csoportok = (from i in list
+                                group i by i into grp
+                                orderby grp.Count() descending
+                                select grp.Key).First();
+                Console.WriteLine($"legtöbször megadott szám a {csoportok}");
+                Console.WriteLine($"alma");
+                Console.ReadLine();
+            }
         }
     }
 }
+
